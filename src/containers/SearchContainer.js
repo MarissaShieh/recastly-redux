341fadfd4942ed4e1ab86eefc93e13bbee40bac9
MarissaShieh <marissa.shieh@gmail.com>
2019-03-06 21:34:35 -0800

    Progress on Search container

--- src/containers/SearchContainer.js
+++ src/containers/SearchContainer.js
@@ -1,8 +1,56 @@
 import React from 'react';
-import Search from './../components/Search.js';
+// import Search from './../components/Search.js';
 import { connect } from 'react-redux';
 import handleSearchChange from '../actions/search.js';
-var SearchContainer = () => {};
+import handleVideoSearch from '../actions/search.js';
+// var SearchContainer = (state) => {
+//     return (
+//         <div className="search-bar form-inline">
+//           <input
+//             className="form-control"
+//             type="text"
+//             value={state.value}
+//             onChange={state.handleSearchChange}
+//           />
+//           <button className="btn hidden-sm-down">
+//             <span className="glyphicon glyphicon-search"></span>
+//           </button>
+//         </div>
+//       );
+// };
+
+class Search extends React.Component {
+  
+    handleInputChange(e) {
+      this.props.handleSearchInputChange(e.target.value);
+      this.setState({
+        value: e.target.value
+      });
+    }
+  
+    render() {
+      return (
+        <div className="search-bar form-inline">
+          <input
+            className="form-control"
+            type="text"
+            value={this.state.value}
+            onChange={this.handleInputChange.bind(this)}
+          />
+          <button className="btn hidden-sm-down">
+            <span className="glyphicon glyphicon-search"></span>
+          </button>
+        </div>
+      );
+    }
+  };
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        onChange: (value) => dispatch(handleVideoSearch(value))
+    }
+}
+
 
 //TODO: define a SearchContainer component which will hook up your action
 // dispatchers with your search component props.
